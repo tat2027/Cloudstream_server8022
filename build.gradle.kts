c38ad4122fac848ede0c2db33fea8e94fa2dbce1
M <102477479+tat2027@users.noreply.github.com>
2025-09-22 10:39:22 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("com.android.library") version("7.4.2") apply false
+    kotlin("android") version "1.8.10" apply false
+}
+
+// NOTE: Adjust plugin versions to match your Android Studio / Cloudstream environment.
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "cloudstream-server8022-extension"
